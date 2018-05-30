f39e06d1a05b6c6d52add763fdfe87b84e197446
Divinariuz <b6099@oic.jp>
2018-05-30 18:44:30 +0900

    5. Master/Detail Components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +8,17 @@ import { HEROES } from "../mock-heroes";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  heroes = HEROES;
+  selectedHero: Hero;
+
   constructor() { }
   ngOnInit() {
   }
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  };
 
-  heroes = HEROES;
-  selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
